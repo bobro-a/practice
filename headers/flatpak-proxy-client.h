925ca52e9b7cc307df5072ac9e662fe3952c7437
bobro-a <m3_ba@mail.ru>
2025-07-12 13:15:15 +0300

    add some methods and 2 constructors into Filter

--- headers/flatpak-proxy-client.h
+++ headers/flatpak-proxy-client.h
@@ -4,36 +4,69 @@
 #include <string>
 #include <vector>
 
-class Filter{
-public:
-    Filter(){
+typedef enum {
+    FILTER_TYPE_CALL = 1 << 0,
+    FILTER_TYPE_BROADCAST = 1 << 1,
+    FILTER_TYPE_ALL=FILTER_TYPE_CALL|FILTER_TYPE_BROADCAST,
+} FilterTypeMask;
 
-    }
+typedef enum {
+    FLATPAK_POLICY_NONE,
+    FLATPAK_POLICY_SEE,
+    FLATPAK_POLICY_TALK,
+    FLATPAK_POLICY_OWN
+} FlatpakPolicy;
+
+class Filter {
+public:
+    Filter(std::string name, bool name_is_subtree, FlatpakPolicy policy);
+
+    Filter(std::string name, bool name_is_subtree, FilterTypeMask types, std::string rule);
+
     std::string name;
+    bool name_is_subtree;
+    FlatpakPolicy policy;
 private:
+    FilterTypeMask types;
+    std::string path;
+    bool path_is_subtree;
+    std::string interface;
+    std::string member;
 };
 
-class FlatpakPolicy {
+class FlatpakProxyClient{
 
-}
+};
 
-class FlatpakProxy{
+class FlatpakProxy {
 public:
     FlatpakProxy();
+
     ~FlatpakProxy();
+
 private:
     bool log_messages;
 //    GList         *clients;
     std::string socket_path;
     std::string dbus_address;
-    bool       filter;
-    bool       sloppy_names;
+    bool filter;
+    bool sloppy_names;
 
-    std::unordered_map<std::string,std::vector<Filter*>> filters;
+    std::unordered_map<std::string, std::vector<Filter *>> filters;
+
     void set_filter(bool filter);
+
     void set_sloppy_names(bool sloopy_names);
+
     void set_log_messages(bool log);
-    void add_filter(Filter* filter);
-    void add_policy(string name, bool name_is_subtree,FlatpakPolicy policy);
+
+    void add_filter(Filter *filter);
+
+    void add_policy(std::string name, bool name_is_subtree, FlatpakPolicy policy);
+
+    void add_call_rule(std::string name, bool name_is_subtree, std::string rule);
+
+    void add_broadcast_rule(std::string name, bool name_is_subtree, std::string rule);
+
 };
 
--- source/flatpak-proxy-client.cpp
+++ source/flatpak-proxy-client.cpp
@@ -1,5 +1,6 @@
 #include "headers/flatpak-proxy-client.h"
 
+
 void FlatpakProxy::set_filter(bool filter) {
     this->filter = filter;
 }
@@ -13,12 +14,69 @@ void FlatpakProxy::set_log_messages(bool log) {
 }
 
 void FlatpakProxy::add_filter(Filter *filter) {
-    this->filters[filter.name].push_back(filter);
+    this->filters[filter->name].push_back(filter);
 }
 
 void
-FlatpakProxy::add_policy(string name,
+FlatpakProxy::add_policy(std::string name,
                          bool name_is_subtree,
                          FlatpakPolicy policy) {
+    Filter *filter = new Filter(name, name_is_subtree, policy); //todo clear
+    this->add_filter(filter);
+}
+
+
+void FlatpakProxy::add_call_rule(std::string name, bool name_is_subtree, std::string rule) {
+    Filter *filter = new Filter(name, name_is_subtree, FILTER_TYPE_CALL, rule);//todo clear
+    this->add_filter(filter);
+}
+
+
+void FlatpakProxy::add_broadcast_rule(std::string name, bool name_is_subtree, std::string rule) {
+    Filter *filter = new Filter(name, name_is_subtree, FILTER_TYPE_BROADCAST, rule);//todo clear
+    this->add_filter(filter);
+}
+
+FlatpakProxy::~FlatpakProxy() {
 
 }
+
+Filter::Filter(std::string name, bool name_is_subtree, FlatpakPolicy policy) {
+    this->name = name;
+    this->name_is_subtree = name_is_subtree;
+    this->policy = policy;
+    this->types = FILTER_TYPE_ALL;
+}
+
+Filter::Filter(std::string name, bool name_is_subtree, FilterTypeMask types, std::string rule) {
+    this->name = name;
+    this->name_is_subtree = name_is_subtree;
+    this->policy = FLATPAK_POLICY_TALK;
+    this->types = types;
+    size_t pos = 0;
+    pos = rule.find('@');
+    if (pos != std::string::npos && pos + 1 < rule.size()) {
+        this->path = rule.substr(pos + 1);
+        if (this->path.ends_with("/*")) {
+            this->path_is_subtree = true;
+            this->path[this->path.size() - 2] = 0;
+        }
+    }
+    size_t method_end = (pos == std::string::npos ? pos : rule.size());
+    if (method_end) {
+        if (rule[0] == '*') {
+
+        } else {
+            this->interface = rule.substr(0, method_end);
+            size_t dot = this->interface.rfind('.');
+            if (dot != std::string::npos) {
+                std::string member = this->interface.substr(dot + 1);
+                if (member != "*") {
+                    this->member = member;
+                }
+                this->interface.resize(dot);
+            }
+        }
+    }
+}
+
