b36db780f797a41d714182d35e3a799e1735ac8d
bobro-a <m3_ba@mail.ru>
2025-07-18 15:42:12 +0300

    release write() and get_signature()

--- source/buffer.cpp
+++ source/buffer.cpp
@@ -53,8 +53,9 @@ bool Buffer::read(ProxySide *side,
         GSocketControlMessage **messages = nullptr;
         int num_messages = 0;
         int flags = 0;
+        GError *error;
 
-        gssize res = g_socket_receive_message(
+        size_t res = g_socket_receive_message(
                 socket,
                 nullptr,
                 &vec, 1,
@@ -62,45 +63,96 @@ bool Buffer::read(ProxySide *side,
                 &num_messages,
                 &flags,
                 nullptr,
-                nullptr
+                &error
         );
-        if (res < 0) return false;
-        if (res == 0) {
+        if (res < 0 && g_error_matches(error, G_IO_ERROR, G_IO_ERROR_WOULD_BLOCK)) {
+            g_error_free(error);
+            return false;
+        }
+
+        if (res <= 0) {
+            if (res != 0) {
+                std::cerr << "Error reading from socket: " << error->message << "\n";
+                g_error_free(error);
+            }
+
             side->side_closed();
             return false;
         }
         received = res;
-        for(int i=0;i<num_messages;++i)
+        for (int i = 0; i < num_messages; ++i)
             control_messages.push_back(messages[i]);
-        g_free (messages);
+        g_free(messages);
     }
-    pos+=received;
+    pos += received;
     return true;
 }
 
-bool write(ProxySide *side,
-           GSocket *socket){
+bool Buffer::write(ProxySide *side,
+                   GSocket *socket) {
+    GError *error;
+    if (send_credentials && G_IS_UNIX_CONNECTION(side->connection)) {
+        assert(size == 1);
+        if (!g_unix_connection_send_credentials(G_UNIX_CONNECTION (side->connection),
+                                                nullptr,
+                                                &error)) {
+            if (g_error_matches(error, G_IO_ERROR, G_IO_ERROR_WOULD_BLOCK)) {
+                g_error_free(error);
+                return false;
+            }
+            std::cerr << "Error sending credentials" << std::endl;
+            side->side_closed();
+            g_error_free(error);
+            return false;
+        }
+        sent = 1;
+        return true;
     }
 
+    const uint8_t *data_to_send = data.data() + sent;
 
-void set_send_credentials(bool value){
+    std::vector<GSocketControlMessage *> messages(control_messages.begin(), control_messages.end());
+    GOutputVector vec;
+    vec.buffer = const_cast<void *>(reinterpret_cast<const void *>(data_to_send));
+    vec.size = pos - sent;
+    gssize res = g_socket_send_message(
+            socket,
+            nullptr,
+            &vec,
+            1,
+            messages.empty() ? nullptr : messages.data(),
+            static_cast<int>(messages.size()),
+            G_SOCKET_MSG_NONE,
+            nullptr,
+            &error
+    );
+    if (res < 0 && g_error_matches(error, G_IO_ERROR, G_IO_ERROR_WOULD_BLOCK)) {
+        g_error_free(error);
+        return false;
+    }
+    if (res <= 0) {
+        if (res < 0) {
+            std::cerr << "Error writing credentials to socket: " << error->message << "\n";
+            g_error_free(error);
+        }
 
+        side->side_closed();
+        return false;
+    }
+    control_messages.clear();
+    sent += res;
+    return true;
 }
 
-bool should_send_credentials();
+std::string Buffer::get_signature(uint32_t &offset, uint32_t end_offset) {
+    if (offset >= end_offset) return FALSE;
+    size_t len=data[offset++];
+    if (offset+len+1>end_offset || data[offset+len]!=0) return FALSE;
+    std::string str(data.begin()+offset,data.begin()+offset+len);
+    offset+=len+1;
+    return str;
+}
 
-size_t get_pos();
+std::string Buffer::get_string(Header *header, uint32_t offset, uint32_t end_offset, GError **error){
 
-void set_pos(size_t p);
-
-size_t get_size();
-
-void set_size(size_t s);
-
-size_t get_sent();
-
-void append_control_message(GSocketControlMessage *msg);
-
-const uint8_t *get_data();
-
-uint8_t *get_data_mutable();
+}
