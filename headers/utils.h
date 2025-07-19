b66d08a7a7d3db8c89720c7015d57842e3c25350
bobro-a <m3_ba@mail.ru>
2025-07-19 15:32:40 +0300

    moved common functions

--- headers/utils.h
+++ headers/utils.h
@@ -1,4 +1,8 @@
 #pragma once
 #include "headers/flatpak-proxy-client.h"
 
-static uint32_t read_uint32(Header* header, std::vector<uint8_t>::iterator it);
+static uint32_t read_uint32(Header* header, uint8_t *ptr);
+
+static inline uint32_t align_by_8 (uint32_t offset);
+
+static inline uint32_t align_by_4(uint32_t offset);
--- source/utils.cpp
+++ source/utils.cpp
@@ -7,3 +7,13 @@ static uint32_t read_uint32(Header* header, uint8_t *ptr) {
            ? GUINT32_FROM_BE (*(guint32 *) ptr)
            : GUINT32_FROM_LE (*(guint32 *) ptr);
 }
+
+static inline uint32_t align_by_8 (uint32_t offset)
+{
+    return (offset + 8 - 1) & ~(8 - 1);
+}
+
+static inline uint32_t
+align_by_4(uint32_t offset) {
+    return (offset + 4 - 1) & ~(4 - 1);
+}
