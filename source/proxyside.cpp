4b846a7ec766763ee2f1f0e242602a3d7bfdd7b6
bobro-a <m3_ba@mail.ru>
2025-07-20 17:03:30 +0300

    realize side_closed,get_other_side and got_buffer_from_side

--- source/proxyside.cpp
+++ source/proxyside.cpp
@@ -40,9 +40,34 @@ ProxySide::~ProxySide() {
     expected_replies.clear();
 }
 
-//todo
-void side_closed();
-void got_buffer_from_side(Buffer* buffer);
+void ProxySide::side_closed(){
+    ProxySide* other_side=this->get_other_side();
+    if (closed) return;
+
+    GSocket *socket=g_socket_connection_get_socket (connection);
+    g_socket_close (socket, nullptr);
+    closed=true;
+
+    GSocket *other_socket = g_socket_connection_get_socket (other_side->connection);
+    if (!other_side->closed && other_side->buffers.empty()){
+        g_socket_close (other_socket, nullptr);
+        other_side->closed = true;
+    }
+    if (other_side->closed) delete client;
+    else{
+        GError *error = nullptr;
+        if (!g_socket_shutdown(other_socket, TRUE, FALSE, &error)) {
+            std::cerr << "Unable to shutdown read side: " << error->message << "\n";
+            g_error_free(error);
+        }
+    }
+}
+
+void ProxySide::got_buffer_from_side(Buffer* buffer){
+    if (this==&client->client_side)
+        client->got_buffer_from_client(buffer);
+    else client->got_buffer_from_bus(buffer);
+}
 
 ProxySide* ProxySide::get_other_side(){
     FlatpakProxyClient *client = this->client;
